4841920a994865087cf4d26b2667935248b67d05
WhaleMilk <collinmbarnett@gmail.com>
2025-12-16 15:03:29 -0700

    basic state tracking with enum

--- src/app/ui.rs
+++ src/app/ui.rs
@@ -1,14 +1,36 @@
 use eframe::egui::CentralPanel;
+use eframe::egui::Window;
 use egui::Context;
 use egui::SidePanel;
+use egui::TextEdit;
 use egui::TextStyle;
 use egui::TopBottomPanel;
 use egui::{FontFamily, FontId};
 use egui_plot::{Line, Plot, PlotPoints};
 
-#[derive(Default)]
 pub struct App {
     username: String, //player username as they would input, ie WhaleMilk#PHUD
+    state: State,     //Coul
+    test_bool: bool,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Default)]
+enum State {
+    #[default]
+    Home,
+    Stats,
+    Profile,
+    Loading,
+}
+
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            username: String::default(),
+            state: State::Home,
+            test_bool: false,
+        }
+    }
 }
 
 impl eframe::App for App {
@@ -17,25 +39,41 @@ impl eframe::App for App {
         top_bar(ctx);
         //CentralPanel::default().show(ctx, |vi| vi.heading("JadeStats"));
         CentralPanel::default().show(ctx, |ui| {
-            ui.vertical_centered_justified(|ui| {
-                ui.set_max_width(200.0);
-                ui.label("Username#PID");
-                ui.add(egui::TextEdit::singleline(&mut self.username).clip_text(true));
+            //match self.state {
+            //    State::Home =>
+            //}
+            if self.state == State::Home {
+                ui.vertical_centered_justified(|ui| {
+                    ui.set_max_width(200.0);
+                    ui.label("Enter Username");
+                    let input_box = ui.text_edit_singleline(&mut self.username);
+                    if input_box.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
+                        self.state = State::Stats;
+                    }
+                    if ui.button("Go!").clicked() {
+                        self.state = State::Stats;
+                    }
+                });
+            }
 
-                ui.set_max_width(600.0);
+            if self.state == State::Stats {
+                ui.vertical_centered_justified(|ui| {
+                    ui.label(format!("Current username: {}", self.username));
+                    ui.set_max_width(600.0);
 
-                let sin: PlotPoints = (0..100)
-                    .map(|i| {
-                        let x = i as f64 * 0.1;
-                        [x, x.sin()]
-                    })
-                    .collect();
-                let line = Line::new("sin_wave", sin);
+                    let sin: PlotPoints = (0..100)
+                        .map(|i| {
+                            let x = i as f64 * 0.1;
+                            [x, x.sin()]
+                        })
+                        .collect();
+                    let line = Line::new("sin_wave", sin);
 
-                Plot::new("test_plot")
-                    .view_aspect(2.0)
-                    .show(ui, |plot_ui| plot_ui.line(line));
-            });
+                    Plot::new("test_plot")
+                        .view_aspect(2.0)
+                        .show(ui, |plot_ui| plot_ui.line(line));
+                });
+            }
         });
 
         SidePanel::left("FileList")
@@ -45,6 +83,10 @@ impl eframe::App for App {
             .show(ctx, |ui| {
                 ui.label("Saved Profiles");
             });
+
+        Window::new("TestWindow").show(ctx, |ui| {
+            ui.label("Test Window");
+        });
     }
 }
 
